d3a9a2e0497463a868a27ab49a0d7dc8a28d4a05
nhanncv <vinhnhan1999bc@gmail.com>
2020-06-13 08:22:42 +0700

    add colors add colors to assets

---
+++ app/assets/styles/colors/index.js
@@ -0,0 +1,17 @@
+const colors = {
+  white: '#fff',
+  blueNavy: '#001730',
+  blueBorder: '#416D8E',
+  blueButton: '#4A83F2',
+  blueText: '#2D6D8B',
+  grayText: '#5D6E78',
+  graySmallText: '#ABBBC0',
+  black: '#4E4E4E',
+  purpleCardVoc: '#4F42D8',
+  purpleVolume: '#6A60DE',
+  greenNormal: '#48C690',
+  red: '#EB002B',
+  yellow: '#FCA92E',
+};
+
+export default colors;
